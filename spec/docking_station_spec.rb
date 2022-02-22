a703ef1a9b349444da4bbb17de4ccea87baa935a
Harry <harrydawson90@hotmail.co.uk>
2022-02-22 15:51:34 +0000

    added test for docking station responds to release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,12 @@
 require 'docking_station'
 
 describe DockingStation do
-    # it "creates an object" do
-    #     docking_station = DockingStation.new
-    # end
+    it "creates an object" do
+        docking_station = DockingStation.new
+    end
     
+    it "responds to release_bike" do
+        docking_station = DockingStation.new.respond_to?('release_bike')
+    end
+
 end
