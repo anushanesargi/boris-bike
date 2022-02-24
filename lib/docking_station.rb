c9a753978aa33ea95f5d9ce58d0cb9d8d9c8af6c
PhilBligh <philbligh@hotmail.com>
2022-02-24 15:21:13 +0000

    Finished step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,23 +4,28 @@ class DockingStation
   
   attr_reader :bike
   
-#   def intialize(capacity)
-#     @bike = []
-#   end
+  def initialize
+    @capacity = 20
+    @bike = []
+  end
 
   def release_bike
-    fail 'No bikes available' unless bike
-    @bike
+    fail 'No bikes available' if @bike.length == 0
+    @bike.pop
   end
 
   def dock(bike)
-    @bike = bike
-    fail 'full' if @bike
-    
-    
+    fail 'full' if @bike.length >= @capacity
+    @bike << bike
   end
 
     
 end
 
 
+# docking_station = DockingStation.new
+# bike = Bike.new
+# 21.times { 
+#   docking_station.dock(bike) 
+# }
+# puts docking_station.bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,32 +13,26 @@ describe DockingStation do
 
     it "takes a bike and expects it can dock" do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
 
     it "returns docked bike" do
         bike = Bike.new
         subject.dock(bike)
-        expect(subject.bike).to eq bike        
-    end
-
-    it "raises error when empty" do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.release_bike).to eq bike
+        expect(subject.bike).to eq [bike]        
     end
 
     describe "release bike error" do
         it "raises an error when there are no bikes available" do
-            expect { subject.release_bike }.to raise_error 'No bikes available'
+            expect { 20.times { subject.release_bike } }.to raise_error 'No bikes available'
         end
     end
 
-    describe ".dock erro" do
-        it ".dock raises an error when full" do
+    describe ".dock error" do
+        it ".dock raises an error when it's at the capacity of 20 bikes" do
             bike = Bike.new
             # subject.dock(bike)
-            expect { subject.dock(bike) }.to raise_error 'full'
+            expect { 21.times { subject.dock(bike) } }.to raise_error 'full'
         end
     end
 
